23fe7010edb9afbe6f0f56f4c01ad366e4980e35
Ajay-101 <ajay.patil@anuyat.com>
2024-05-18 20:55:16 +0530

    error Handel 2

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 require('dotenv').config();
 const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-const app = express();
-const port = process.env.PORT || 5000;
+// Add CORS middleware
+app.use(cors());
 
-const startServer = async () => {
-  try {
-    // Connect to MongoDB
-    await connectDB();
+connectDB();
 
-    // Add CORS middleware
-    app.use(cors());
 
-    // Body parser middleware
-    app.use(express.json());
+app.use(express.json());
 
-    // Routes
-    const authRoutes = require('./Server/routes/routes');
-    app.use('/', authRoutes);
 
-    // Start the server
-    app.listen(port, () => {
-      console.log(`App listening on port ${port}`);
-    });
-  } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
-    process.exit(1);  // Exit the process with an error code
-  }
-};
+const authRoutes = require('./Server/routes/routes');
+app.use('/', authRoutes);
 
-startServer();
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
